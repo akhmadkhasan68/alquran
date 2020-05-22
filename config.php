a8bf4781ae0ac6de55807d88884640f4dbc8cb2a
akhmadkhasan68 <41146877+akhmadkhasan68@users.noreply.github.com>
2020-05-22 13:46:26 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,22 @@
+﻿<?php
+$themes = 'simple';
+$mysql_host = 'localhost';
+$mysql_user = 'root';
+$mysql_password ='';
+
+$mysql_database = 'quran';
+error_reporting(0);
+
+DEFINE("SOUND","sound");
+DEFINE("DEFAULT_MOD","quran.daftar_sura.show");
+DEFINE("UN","haekal");
+DEFINE("PW","vita");
+
+$link = mysql_connect($mysql_host,$mysql_user,$mysql_password);
+mysql_set_charset("utf8",$link);
+mysql_select_db($mysql_database); 
+
+include("includes/fungsi.php");
+
+
+?>

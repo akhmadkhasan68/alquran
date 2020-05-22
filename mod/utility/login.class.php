9d5009ee9ff1f186dfc0dc68ff42d0ea015bb89b
akhmadkhasan68 <41146877+akhmadkhasan68@users.noreply.github.com>
2020-05-22 14:38:29 +0700

    second commit

--- mod/utility/login.class.php
+++ mod/utility/login.class.php
@@ -3,27 +3,42 @@ class login{
 
 	function show(){
 		echo "
-		<form method='post' action='?mod=utility.login.cek' >
-			<table style='border:solid 1px brown;font-family:verdana;font-size:11px;line-height:180%;padding:10px' align='center'>
-				<tr>
-					<td>Username </td>
-					<td>:</td>
-					<td><input name='txt_username' ></td>
-				</tr>
-				<tr>
-					<td>Password </td>
-					<td>:</td>
-					<td><input type='password' name='txt_password' ></td>
-				</tr>
-				<tr>
-					<td colspan='3' align='right'>
-						<input type='submit' value='login'>
-					</td>
-				</tr>
-				
-			</table>
-			
-		</form>
+		<div class='row'>
+			<div class='col-md-12 col-12'>
+				<form method='post' action='?mod=utility.login.cek' >
+					<div class='card'>
+						<div class='card-header'>
+							<h4>Login</h4>
+						</div>
+						<div class='card-body'>
+							<div class='row'>
+								<div class='col-md-3 col-4'>
+									<strong>Username</strong>
+								</div>
+								<div class='col-md-9 col-8'>
+									<input name='txt_username' class='form-control'>
+								</div>
+							</div>
+
+							<div class='row mt-3'>
+								<div class='col-md-3 col-4'>
+									<strong>Password</strong>
+								</div>
+								<div class='col-md-9 col-8'>
+									<input type='password' name='txt_password' class='form-control'>
+								</div>
+							</div>
+
+							<div class='row mt-3'>
+								<div class='col-md-12 col-12'>
+									<input type='submit' value='login' class='btn btn-primary btn-block'>
+								</div>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
 		";
 	}
 	
